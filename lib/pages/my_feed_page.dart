c898b2df2e512fc38271807b1c6baba4a8fcd7ec
Zafarbek <zzaaffaarr333@gmail.com>
2023-08-25 12:43:59 +0500

    likes

--- lib/pages/my_feed_page.dart
+++ lib/pages/my_feed_page.dart
@@ -126,7 +126,7 @@ class _MyFeedPageState extends State<MyFeedPage> {
                   IconButton(
                     onPressed: (){},
                     icon: Icon(
-                      EvaIcons.heart,
+                      EvaIcons.heartOutline,
                       color: Colors.red,
                     ),
                   ),
